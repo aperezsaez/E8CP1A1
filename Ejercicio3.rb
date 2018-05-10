5d22aa6d5a44b553feb7bd8250088020021a5eca
aperezsaez <alex.perez.22@live.com>
2018-05-09 21:38:22 -0300

    Ejercicio3

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -1,3 +1,6 @@
+#Crear un método que reciba el archivo peliculas.txt, lo abra y cuente la cantidad total
+#de palabras. El método debe devolver este valor.
+
 def arr(word)
 file = File.open(word, 'r')
  data = file.read.split(' ').map(&:chomp)
@@ -6,3 +9,17 @@ file = File.open(word, 'r')
 end
 
 arr('peliculas.txt')
+
+#Crear un método similar para que además reciba -como argumento- un string. En
+#este caso el método debe contar únicamente las apariciones de ese string en el
+#archivo
+
+def arr(archivo, string)
+  file = File.open(archivo, 'r')
+  data = file.read
+  num = data.scan(string).count
+  puts num
+  file.close
+end
+
+arr('peliculas.txt', 'la')
